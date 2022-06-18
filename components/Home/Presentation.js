2c781cd3d7d66c408ddacaf57b314c6b1a69bbba
Edwin Cacuango <edwin-fernando@hotmail.es>
2022-06-18 14:17:47 -0500

    Added link to btn component in presentation section

--- components/Home/Presentation.js
+++ components/Home/Presentation.js
@@ -8,7 +8,7 @@ export const Presentation = () => {
         <h1 className="title">Edwin Cacuango</h1>
         <p className="text-enphasis">Utilizo la tecnología para hacer más fácil el acceso a los servios legales</p>
         <p>Soy egresado de Derecho y también soy desarrollador frontend. Te puedo ayudar en dos tipos de servicios: legales y de desarrollo web. Si quieres mezclar un poco las cosas, también puedo ayudarte en asesoría legal relacionada con la web o podemos crear juntos una app legal.</p>
-        <Button stylesAdded="my-3">Escríbeme!</Button>
+        <Button stylesAdded="my-3"><a href="/#contact">Escríbeme</a></Button>
       </div>
 
     </section>
