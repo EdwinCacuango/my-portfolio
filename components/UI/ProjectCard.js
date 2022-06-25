917d22cea5b1f86f8870bd718f8690ee60c338e8
Edwin Cacuango <edwin-fernando@hotmail.es>
2022-06-24 19:33:04 -0500

    Changed grid template areas for simple grid That's for add an empty space betwen sidebar and main

--- components/UI/ProjectCard.js
+++ components/UI/ProjectCard.js
@@ -3,13 +3,13 @@ import GitHub from "../../public/Imgs/github-logo.svg"
 import site from "../../public/Imgs/enlace-externo.svg"
 export const ProjectCard = ({ image, titleProject,description, linkSite, gitHubRepo }) => {
     return (
-        <div className="card text-center">
+        <div className="card text-center pt-6">
             <Image src={image} alt="" />
             <div>
                 <p className="text-enphasis">{titleProject}</p>
                 <p className="text-left px-1 text-base">{description}</p>
             </div>
-            <div className="mt-2 flex gap-1 items-center">
+            <div className="self-end mt-2 flex gap-1 items-center">
                 <a className="icon-card" src={linkSite}><Image src={site} alt="Linkt to site" width={25} height={25}/></a>
                 <a className="icon-card" src={gitHubRepo}><Image src={GitHub} alt="Link to GitHub repository" width={45} height={45}/></a>
             </div>
