f107d031b8e2a60c8a31b31e388cbe29ce39cf7c
Edwin Cacuango <edwin-fernando@hotmail.es>
2022-06-25 21:26:44 -0500

    Changed dimensions of every component and added grid for located text content in every card.

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -11,11 +11,14 @@ const BlogCard = ({ cardStyle, title, titleStyle, coverPhoto, datePublished, slu
                     layout="fill"
                 />
             </div>
-            <div className="mt-2">
+            <div className="mt-2 grid grid-rows-3 gap-2">
                 <p className="text-xs my-2"><span className="bg-rose-500 px-3 py-1 rounded-xl text-slate-100">{category}</span> • {datePublished}</p>
-                <h2 className={titleStyle}>{title}</h2>
-                <p className="text-xs">{description}</p>
-                <div className="text-sm text-right">
+                <div className="place-self-center">
+                    <Link href={`/posts/${slug}`}><h3 className={`${titleStyle} hover:text-rose-400`}>{title}</h3></Link>
+                    <p className="text-base">{description}</p>
+
+                </div>
+                <div className="text-sm mx-3 text-right text-rose-500 hover:text-rose-300">
                     <Link href={`/posts/${slug}`}><a>Seguir leyendo →</a></Link>
                 </div>
             </div>
