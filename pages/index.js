82f299f0c6c38ce3f033c8ca90805da7a2c4c110
Edwin Cacuango <edwin-fernando@hotmail.es>
2022-07-05 10:42:59 -0500

    Hiding section projects until the information is ready

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
           <ExperienceTable isLegal={isLegal} />
         </div>
       </section>
-      <Projects />
+      {/* <Projects /> */}
       <FormContact />
     </Layout>
 
