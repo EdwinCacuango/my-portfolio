15ea7b8aa5753d88124a86d7adeddd30325830cd
Edwin Cacuango <edwin-fernando@hotmail.es>
2022-07-14 15:57:40 -0500

    Buy me a coffe link - Bug: Some friend report that "Buy me a coffe" website had     some problems to accept his credit card. - Solution: To avoid future problems, the btn buy me a coffe and     the text in slug component was changed to incorporate     PayPalMe link. - Also added a PayPal Icon in the btn

--- components/BuyMeACoffe.js
+++ components/BuyMeACoffe.js
@@ -1,11 +1,14 @@
 const BuyMeACoffe = () => {
     return (
         <a
-            href="https://www.buymeacoffee.com/edwinfcacuango"
+            href="https://paypal.me/edwfc?country.x=EC&locale.x=es_XC"
             target="_blank"
             rel="noreferrer"
-            className="md:text-xl btn-primary rounded-lg">
-            Invítame un café ☕
+            className="md:text-xl btn-primary rounded-lg flex gap-3 items-center max-w-content">
+            Donaciones por
+            <div className="w-6">
+                <img clasname="max-w-full" src="/paypal.png" alt="Paypal icon" />
+            </div>
         </a>
     )
 }
