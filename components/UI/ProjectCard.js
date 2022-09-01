b7f6ae26fffd15108c0d4401fa75ff5f35ec9816
Edwin Cacuango <edwin-fernando@hotmail.es>
2022-08-31 21:02:39 -0500

    Fixed padding on card

--- components/UI/ProjectCard.js
+++ components/UI/ProjectCard.js
@@ -3,11 +3,11 @@ import GitHub from "../../public/Imgs/github-logo.svg"
 import site from "../../public/Imgs/enlace-externo.svg"
 export const ProjectCard = ({ image, titleProject, description, linkSite, gitHubRepo }) => {
     return (
-        <div className="card rounded-lg text-center pt-6">
+        <div className="card rounded-lg text-center pt-6 px-4">
             <img src={image} alt={`${titleProject} homepage`} />
             <div className="my-4">
                 <a className="text-2xl text-slate-500 md:text-enphasis hover:text-rose-500" href={linkSite}>{titleProject}</a>
-                <p className="text-left px-2 md:px-4 pt-3 text-base">{description}</p>
+                <p className="text-left px-2 md:px-3 pt-3 text-base">{description}</p>
             </div>
             <div className="m-4 self-end mt-2 flex gap-1 items-center">
                 <a className="icon-card" href={linkSite} target="_blank" rel="noreferrer"><Image src={site} alt="Linkt to site" width={25} height={25} /></a>
